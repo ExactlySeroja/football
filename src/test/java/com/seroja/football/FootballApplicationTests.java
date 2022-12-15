4aeddbc9a409e005b77a7584b8d66a6fff75b851
Seroja <sersergey04@gmail.com>
2022-12-15 16:42:20 +0200

    Successfully created web application "football" 1.0

---
+++ src/test/java/com/seroja/football/FootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seroja.football;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
